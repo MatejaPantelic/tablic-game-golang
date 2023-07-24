8715839e72d62800dbb42681a2e266f2fdb85652
BenjaminHrustemovic-hub <hrustemovic.benjamin@gmail.com>
2023-07-24 14:34:07 +0200

    All PR corrections have been made

--- api/handlers.go
+++ api/handlers.go
@@ -2,47 +2,54 @@ package api
 
 import (
 	"encoding/json"
-	//"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"main.go/constants"
 	"main.go/initializers"
 	"main.go/models"
+	"fmt"
 )
 
+// Making the queue where player will be stored until game starts
 var queue []models.User
 
+//Helper function for error responses
+func errorCheck( err error, errCode int,errMsg string ,c *gin.Context ){
+	if err != nil {
+		c.JSON(errCode, gin.H{"message": errMsg})
+		return
+	}
+}
+
+// Function- adds player to queue.
+// Creates a new user according to model, checks for possible errors, adds user to database and to queue
+// If enough players are in queue, remove those player from queue and calls function startGame
 func addPlayerHandler(c *gin.Context) {
 	var newUser models.User
 
 	err := c.BindJSON(&newUser)
-	if err != nil {
-		c.JSON(400, gin.H{"message": "Invalid user data"})
-		return
-	}
+	errorCheck(err,400,"Invalid user data",c)
 
 	result := initializers.DB.Create(&newUser)
-	if result.Error != nil {
-		c.JSON(500, gin.H{"error": "Failed to create user"})
-		return
-	}
+	errorCheck(result.Error,http.StatusBadRequest,"Failed to create user",c)
 
 	queue = append(queue, newUser)
 	if len(queue) >= 2 {
-		queue = queue[:2]
-		c.JSON(201, gin.H{"message": "Waiting to start the game"})
 		startGame(queue[0], queue[1], c)
+		queue = queue[:2]
 	} else {
 		c.JSON(201, gin.H{"message": "Wainting for the other player to join"})
 	}
 }
 
+// Called by addPlayerHandler
+// If enough players are present, starts the game
+// Creates a game, deck and piles (player hands and table cards)
 func startGame(player1 models.User, player2 models.User, c *gin.Context) {
-	// alociram deck
-	response, err := http.Get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-	if err != nil {
-		c.JSON(500, gin.H{"message": "Error starting the game"})
-		return
-	}
+	// Deck creation/alocation
+	errorResponse := "Error starting the game"
+	response, err := http.Get(constants.NEW_SHUFFLED_DECK)
+	errorCheck(err,http.StatusBadRequest,errorResponse,c)
 	defer response.Body.Close()
 
 	if response.StatusCode == http.StatusOK {
@@ -51,12 +58,9 @@ func startGame(player1 models.User, player2 models.User, c *gin.Context) {
 		}
 
 		err = json.NewDecoder(response.Body).Decode(&deckResponse)
-		if err != nil {
-			c.JSON(500, gin.H{"message": "Error starting the game"})
-			return
-		}
+		errorCheck(err,http.StatusBadGateway,errorResponse,c)
 
-		//pravim gejm
+		//Game creation
 		newGame := models.Game{
 			Score:         0,
 			DeckPile:      deckResponse.DeckID,
@@ -68,11 +72,8 @@ func startGame(player1 models.User, player2 models.User, c *gin.Context) {
 			User:          player1,
 		}
 		result := initializers.DB.Create(&newGame)
-		if result.Error != nil {
-			c.JSON(500, gin.H{"message": "Error starting the game"})
-			return
-		}
-
+		errorCheck(result.Error,500,errorResponse,c)
+		
 		newGame2 := models.Game{
 			Score:         0,
 			DeckPile:      deckResponse.DeckID,
@@ -84,176 +85,74 @@ func startGame(player1 models.User, player2 models.User, c *gin.Context) {
 			User:          player2,
 		}
 		result2 := initializers.DB.Create(&newGame2)
+
 		if result2.Error != nil {
-			c.JSON(500, gin.H{"message": "Error starting the game"})
+			c.JSON(500, gin.H{"message": errorResponse})
 			return
 		} else {
 			c.JSON(201, gin.H{"message": "Game has started", "game1": newGame, "game2": newGame2})
 		}
 
-		//Uzimanje karata iz spila, formiranje karata za prvog igraca
-		URL := "https://www.deckofcardsapi.com/api/deck/"+deckResponse.DeckID+"/draw/?count="
-		URL1 := URL+"6"
-		drawnCards, err := http.Get(URL1)
-		if err != nil {
-			c.JSON(500, gin.H{"message": "Error starting the game"})
-			return
-		}
-		defer drawnCards.Body.Close()
+		//Taking 6 cards from deck and forming cards for player1 hands
+		createPile("6",deckResponse.DeckID,newGame.HandPile,c)
 		
-		//Karte se dodjeljuju u hand pile
-		if drawnCards.StatusCode == http.StatusOK{
+		//Taking 6 cards from deck and forming cards for player2 hands
+		createPile("6",deckResponse.DeckID,newGame2.HandPile,c)
 
-			var drawnCardsResponse struct {
-				Success   bool   `json:"success"`
-				DeckId    string `json:"deck_id"`
-				Cards     []models.Card `json:"cards"`
-				Remaining int    `json:"remaining"`
-			}
+		//Taking 4 cards from deck and forming cards for table
+		createPile("4",deckResponse.DeckID,newGame.TablePile,c)
+
+	} else {
+		c.JSON(500, gin.H{"message": errorResponse})
+	}
+}
+
+//Used to create 3 piles at the start of game
+//Those 3 piles are player hands(for each player) and table pile
+func createPile(numberOfCards string, deckID string, pileToBeCreated string,c *gin.Context) {
+	draw_A_Card := fmt.Sprintf(constants.DRAW_A_CARD_URL, deckID, numberOfCards)
+	drawnCards, err := http.Get(draw_A_Card)
+	errorMessage:= "Error starting the game"
+	errorCheck(err,500,errorMessage,c)
+	defer drawnCards.Body.Close()
 			
-			err = json.NewDecoder(drawnCards.Body).Decode(&drawnCardsResponse)
+			
+	if drawnCards.StatusCode == http.StatusOK{
+	
+		var drawnCardsResponse struct {
+			Success   bool   `json:"success"`
+			DeckId    string `json:"deck_id"`
+			Cards     []models.Card `json:"cards"`
+			Remaining int    `json:"remaining"`
+		}
+				
+		err = json.NewDecoder(drawnCards.Body).Decode(&drawnCardsResponse)
+		errorCheck(err,500,errorMessage,c)
+	
+		//Taking card codes for API URL
+		cardCodes := ""
+		for i := 0; i < len(drawnCardsResponse.Cards); i++ {
+			cardCodes+=drawnCardsResponse.Cards[i].Code
+			if(i < len(drawnCardsResponse.Cards)-1){
+				cardCodes+=","
+			}
+		}
+	
+		addToPileURL := fmt.Sprintf(constants.ADD_TO_PILE_URL, deckID,pileToBeCreated,cardCodes)
+		newPile, err := http.Get(addToPileURL)
+		errorCheck(err,500,errorMessage,c)
+		defer newPile.Body.Close()
+	
+		if newPile.StatusCode == http.StatusOK{
+			var player1HandPileResponse models.AddingToPilesResponse
+					
+			err = json.NewDecoder(newPile.Body).Decode(&player1HandPileResponse)
 			if err != nil {
 				c.JSON(500, gin.H{"message": "Error starting the game"})
 				return
 			}
-
-			//Making of first player hand
-			cardCodes := ""
-            for i := 0; i < len(drawnCardsResponse.Cards); i++ {
-				cardCodes+=drawnCardsResponse.Cards[i].Code
-				if(i < len(drawnCardsResponse.Cards)-1){
-					cardCodes+=","
-				}
-			}
-
-			URL_AddToPiles := "https://deckofcardsapi.com/api/deck/"+deckResponse.DeckID+"/pile/"+newGame.HandPile+"/add/?cards="+cardCodes
-			player1Hand, err := http.Get(URL_AddToPiles)
-			if err != nil {
-		        c.JSON(500, gin.H{"message": "Error starting the game"})
-		        return
-	        }
-	        defer player1Hand.Body.Close()
-
-			if player1Hand.StatusCode == http.StatusOK{
-				var player1HandPileResponse models.AddingToPilesResponse
-				
-				err = json.NewDecoder(player1Hand.Body).Decode(&player1HandPileResponse)
-				if err != nil {
-					c.JSON(500, gin.H{"message": "Error starting the game"})
-					return
-				}
-			}
 		}
-		/*-------------------------------------------------------------------------------------------*/
-				//Uzimanje karata iz spila, formiranje karata za drugog igraca
-				URL2 := URL+"6"
-				drawnCards2, err := http.Get(URL2)
-				if err != nil {
-					c.JSON(500, gin.H{"message": "Error starting the game"})
-					return
-				}
-				defer drawnCards2.Body.Close()
-				
-				//Karte se dodjeljuju u hand pile
-				if drawnCards2.StatusCode == http.StatusOK{
-		
-					var drawnCards2Response struct {
-						Success   bool   `json:"success"`
-						DeckId    string `json:"deck_id"`
-						Cards     []models.Card `json:"cards"`
-						Remaining int    `json:"remaining"`
-					}
-					
-					err = json.NewDecoder(drawnCards2.Body).Decode(&drawnCards2Response)
-					if err != nil {
-						c.JSON(500, gin.H{"message": "Error starting the game"})
-						return
-					}
-		
-					//Making of second player hand
-					cardCodes := ""
-					for i := 0; i < len(drawnCards2Response.Cards); i++ {
-						cardCodes+=drawnCards2Response.Cards[i].Code
-						if(i < len(drawnCards2Response.Cards)-1){
-							cardCodes+=","
-						}
-					}
-		
-					URL_AddToPiles := "https://deckofcardsapi.com/api/deck/"+deckResponse.DeckID+"/pile/"+newGame2.HandPile+"/add/?cards="+cardCodes
-					player2Hand, err := http.Get(URL_AddToPiles)
-					if err != nil {
-						c.JSON(500, gin.H{"message": "Error starting the game"})
-						return
-					}
-					defer player2Hand.Body.Close()
-		
-					if player2Hand.StatusCode == http.StatusOK{
-						var player2HandPileResponse models.AddingToPilesResponse
-		
-						err = json.NewDecoder(player2Hand.Body).Decode(&player2HandPileResponse)
-						if err != nil {
-							c.JSON(500, gin.H{"message": "Error starting the game"})
-							return
-						}
-					}
-				}
-				/*-----------------------------------------------------------------------------------------------------------*/
-				//Uzimanje karata iz spila, formiranje karata za talon
-				URL3 := URL+"4"
-				drawnCardsTable, err := http.Get(URL3)
-				if err != nil {
-					c.JSON(500, gin.H{"message": "Error starting the game"})
-					return
-				}
-				defer drawnCardsTable.Body.Close()
-				
-				//Karte se dodjeljuju u hand pile
-				if drawnCardsTable.StatusCode == http.StatusOK{
-		
-					var drawnCardsTableResponse struct {
-						Success   bool   `json:"success"`
-						DeckId    string `json:"deck_id"`
-						Cards     []models.Card `json:"cards"`
-						Remaining int    `json:"remaining"`
-					}
-					
-					err = json.NewDecoder(drawnCardsTable.Body).Decode(&drawnCardsTableResponse)
-					if err != nil {
-						c.JSON(500, gin.H{"message": "Error starting the game"})
-						return
-					}
-		
-					//Making of table cards
-					cardCodes := ""
-					for i := 0; i < len(drawnCardsTableResponse.Cards); i++ {
-						cardCodes+=drawnCardsTableResponse.Cards[i].Code
-						if(i < len(drawnCardsTableResponse.Cards)-1){
-							cardCodes+=","
-						}
-					}
-		
-					URL_AddToPiles := "https://deckofcardsapi.com/api/deck/"+deckResponse.DeckID+"/pile/"+newGame.TablePile+"/add/?cards="+cardCodes
-					Table, err := http.Get(URL_AddToPiles)
-					if err != nil {
-						c.JSON(500, gin.H{"message": "Error starting the game"})
-						return
-					}
-					defer Table.Body.Close()
-		
-					if Table.StatusCode == http.StatusOK{
-						var TablePileResponse models.AddingToPilesResponse
-		
-						err = json.NewDecoder(Table.Body).Decode(&TablePileResponse)
-						if err != nil {
-							c.JSON(500, gin.H{"message": "Error starting the game"})
-							return
-						}
-					}
-				}		
-
-	} else {
-		c.JSON(500, gin.H{"message": "Error starting the game"})
-	}
+	}	
 }
 
 func InitializeHandlers(router *gin.Engine) {
