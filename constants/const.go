1eaed5c598ea598c45d063fc66523b3085cdd936
unknown <ilic98djordje@gmail.com>
2023-07-24 09:44:21 +0200

    for master branch

--- constants/const.go
+++ constants/const.go
@@ -1,8 +1,8 @@
 package constants
 
 const (
-	NEW_DECK_URL = "https://www.deckofcardsapi.com/api/deck/new/"
-	LIST_PILE_CARDS_URL = "https://www.deckofcardsapi.com/api/deck/%s/pile/%s/list/"
-	ADD_TO_PILE_URL= "https://www.deckofcardsapi.com/api/deck/%s/pile/%s/add/?cards=%s"
+	NEW_DECK_URL             = "https://www.deckofcardsapi.com/api/deck/new/"
+	LIST_PILE_CARDS_URL      = "https://www.deckofcardsapi.com/api/deck/%s/pile/%s/list/"
+	ADD_TO_PILE_URL          = "https://www.deckofcardsapi.com/api/deck/%s/pile/%s/add/?cards=%s"
 	DRAW_CARDS_FROM_PILE_URL = "https://www.deckofcardsapi.com/api/deck/%s/pile/%s/draw/?cards=%s"
 )
--- constants/constants.go
+++
@@ -1,4 +0,0 @@
-package constants
-
-const NewDeckURL = "https://www.deckofcardsapi.com/api/deck/new/"
-const LISTPILECARDS_URL = "https://www.deckofcardsapi.com/api/deck/%s/pile/%s/list/"
