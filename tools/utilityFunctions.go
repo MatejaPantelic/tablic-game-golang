0bb8f08f5e5bed2876823fb0f9e5dedd9bb3e175
Jana Mitrovic <mitrovicjana0@gmail.com>
2023-07-27 15:51:17 +0200

    Deleted util.go

--- tools/utilityFunctions.go
+++ tools/utilityFunctions.go
@@ -236,3 +236,30 @@ func CalculateCardPoints(code string) int {
 	}
 	return 0
 }
+
+func JsonParse(response *http.Response, c *gin.Context, parameter interface{}) error {
+	//part of code for accepting response
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+
+	//check error during reading response
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Error during reading response!"})
+	}
+
+	// error expectation if exist
+	errHandUm := json.Unmarshal(body, parameter)
+
+	return errHandUm
+}
+
+func CheckError(code int, c *gin.Context, err error, message string) {
+
+	if err != nil {
+		if message != "" {
+			c.JSON(code, gin.H{"message": message})
+		} else {
+			c.JSON(code, gin.H{"message": err})
+		}
+	}
+}
